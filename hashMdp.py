a082a040d4bf7aedd1a194d05523003f0e222099
lZiemniak <leonard.ziemniak.sio@gmail.com>
2021-09-22 15:45:26 +0200

    correction script de hash

--- hashMdp.py
+++ hashMdp.py
@@ -1,25 +1,20 @@
 import hashlib
-import binascii
 import os
-
-#!!!!!!!!!!!!!!!!!!!!!!!!
-#!Code pas encore testé!!
-#!!!!!Code à tester!!!!!!
-#!!!!!!!!!!!!!!!!!!!!!!!!
-
 
 
 #Hashage du mot de passe
 def hashMdp(passwd):
-    clef = hashlib.sha256(os.urandom(24)).hexdigest().encode("ascii")
-    passwd_hash = hashlib.pbkdf2_hmac("sha256",passwd.encode("utf-8"), clef, 100000)
-    passwd_hash = binascii.hexlify(passwd_hash)
-    return(clef+passwd_hash).decode("ascii")
+    salt=os.urandom(32)
+    key = hashlib.pbkdf2_hmac("sha256",passwd.encode("utf-8"),salt,100000)
+    return salt+key
 
-#Vérification du bon mot de passe entrée
-def checkMdp(mdp_stored, user_passwd):
-    clef = mdp_stored[:24]
-    mdp_stored = mdp_stored[24:]
-    passwd_hash = hashlib.pbkdf2_hmac("sha256", user_passwd.encode("utf-8"),clef.encode("ascii"),100000)
-    passwd_hash = binascii.hexlify(passwd_hash).decode("ascii")
-    return passwd_hash == user_passwd
+#check si mdp correct
+def checkMdp(storedPasswd, passwd):
+    storedSalt = storedPasswd[:32]
+    storedKey = storedPasswd[32:]
+    passwd = hashlib.pbkdf2_hmac("sha256",passwd.encode("utf-8"),storedSalt,100000)
+    passwd = storedSalt+passwd
+    if storedPasswd == passwd:
+        return True
+    else:
+        return False
